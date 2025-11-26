f6e83a78ef522e726adbacaaa3b9cc4b8ca7b6e1
Daniel Hernández <d.hernandezt.96@gmail.com>
2025-11-26 17:15:50 +0000

    quitar style de apuntes

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -28,8 +28,8 @@ export default function Card({title, description, image}) {
     }
 
     return (
-        <div className="card" style={{width: "9rem;", marginTop: "5vh"}}>
-            <img src={image} className="card-img-top" alt="..." style={{width: "500px", height:"500px"}}/>
+        <div className="card" >
+            <img src={image} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className={"card-title " + colorName}>{animalName ? animalName : 'Default title'}</h5>
                 <p className="card-text"> {description ? description : 'Some quick example text to build on the card title and make up the bulk of the card’s content.'}</p>
